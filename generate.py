3735f82cebe3586f4fa9b965b8667ff8c41812f6
CHIZIE <packation@gmail.com>
2017-11-27 00:35:06 +0900

    impliment file writing

--- generate.py
+++ generate.py
@@ -7,7 +7,7 @@ import random
 f = open('text.txt', 'r')
 data = f.read()
 
-for i in range(5):
+for i in range(5): # 5 times 
 	index =int(np.random.rand()*len(data))
 	times =int(np.random.rand()*3) ## A same charactor appears 3 times...
 	#print(index)
@@ -18,7 +18,9 @@ for i in range(5):
 
 print(data)
 
-
+fw = open('result.txt', 'w') # 書き込みモードで開く
+fw.write(data) # 引数の文字列をファイルに書き込む
+fw.close() # ファイルを閉じる
 
 
 
